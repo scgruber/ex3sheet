888dd1d33e04c7ff8e3d3e6a76b4ea04a3130138
Sam Gruber <grubermensch@gmail.com>
2016-12-24 13:05:48 -0800

    Convert Specialties to React
    This seemed to be one of the more straightforward sections to convert.
    Keeping all components in one file for now since I don't have any
    command line tools on me.

---
+++ components.jsx
@@ -0,0 +1,62 @@
+﻿var BigPanel = React.createClass({
+    propTypes: {
+        title: React.PropTypes.string.isRequired
+    },
+
+    render: function() {
+        return (<section className="big-panel">
+            <header><h2>{ this.props.title }</h2></header>
+            <div className="big-panel-content">
+                { this.props.children }
+            </div>
+        </section>);
+    }
+});
+
+var SpecialtiesPanel = React.createClass({
+    propTypes: {
+        specialties: React.PropTypes.objectOf(React.PropTypes.arrayOf(React.PropTypes.string)).isRequired
+    },
+
+    sortIntoColumns: function(specialties) {
+        var height = Math.ceil(specialties.length / 4) + 1;
+        var columns = [ [], [], [], [] ];
+        for (var i=0; i < specialties.length; i++) {
+            columns[i%4].push(specialties[i]);
+        }
+        for (var j=0; j < 4; j++) {
+            while (columns[j].length < height) {
+                columns[j].push(null);
+            }
+        }
+        return columns;
+    },
+
+    renderColumn: function(column, idx) {
+        return (<div key={ idx } className="flex-1">
+            { column.map(function(specialty) {
+                if (specialty === null) {
+                    return (<div key="null" className="blank-line"></div>);
+                } else {
+                    return (<div key={ specialty } className="ellipsis-overflow">{ specialty }</div>);
+                }
+            }) }
+        </div>);
+    },
+
+    render: function () {
+        var self = this;
+        var flatSpecialties = [];
+        Object.keys(this.props.specialties).sort().forEach(function(a) {
+            flatSpecialties = flatSpecialties.concat(self.props.specialties[a].sort().map(function(s) {
+                return a + ": " + s;
+            }));
+        });
+        var columns = this.sortIntoColumns(flatSpecialties)
+        return (<BigPanel title="Specialties">
+          <div className="flex-container">
+            { columns.map(function(column, idx) { return self.renderColumn(column, idx) }) }
+          </div>
+        </BigPanel>);
+    }
+});
