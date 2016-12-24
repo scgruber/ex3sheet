1d400083f9d322b2e7f620cf53f68d2cc6cae5b7
Sam Gruber <grubermensch@gmail.com>
2016-12-24 15:01:33 -0800

    Convert Attributes to React

--- components.jsx
+++ components.jsx
@@ -30,7 +30,7 @@ var Dots = React.createClass({
                 output += openDot;
             }
         }
-        return (<span>{ output }</span>);
+        return (<span className="dots">{ output }</span>);
     }
 });
 
@@ -47,8 +47,44 @@ var DottedStat = React.createClass({
               : (<div className="stat blank-line flex-1"></div>) }
             <div className="rating"><Dots fill={ this.props.rating } max={ 5 } /></div>
         </div>);
+}
+});
+
+var AttributesPanel = React.createClass({
+    propTypes: {
+        attributes: React.PropTypes.shape({
+            'Strength': React.PropTypes.number.isRequired,
+            'Dexterity': React.PropTypes.number.isRequired,
+            'Stamina': React.PropTypes.number.isRequired,
+            'Charisma': React.PropTypes.number.isRequired,
+            'Manipulation': React.PropTypes.number.isRequired,
+            'Appearance': React.PropTypes.number.isRequired,
+            'Perception': React.PropTypes.number.isRequired,
+            'Intelligence': React.PropTypes.number.isRequired,
+            'Wits': React.PropTypes.number.isRequired,
+        }).isRequired
+    },
+
+    render: function() {
+        var self = this;
+        var columns = [
+            ['Strength', 'Dexterity', 'Stamina'],
+            ['Charisma', 'Manipulation', 'Appearance'],
+            ['Perception', 'Intelligence', 'Wits']
+        ];
+        return (<BigPanel title="Attributes">
+            <div className="flex-container">
+                { columns.map(function(col) {
+                    return (<div key={ col.join() } className="flex-1">
+                        { col.map(function(attr) {
+                            return (<DottedStat key={ attr } stat={ attr } rating={ self.props.attributes[attr] }/>);
+                        }) }
+                    </div>);
+                }) }
+            </div>
+        </BigPanel>)
     }
-})
+});
 
 var SpecialtiesPanel = React.createClass({
     propTypes: {
