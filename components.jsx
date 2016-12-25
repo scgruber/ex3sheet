2124115601e7952b2cde1723af6fc87d8f1c4594
Sam Gruber <grubermensch@gmail.com>
2016-12-25 04:55:55 -0800

    Convert Attacks to React

--- components.jsx
+++ components.jsx
@@ -44,10 +44,44 @@ var DottedStat = React.createClass({
         return (<div className="flex-container">
             { this.props.stat ?
                 (<div className="stat ellipsis-overflow flex-1">{ this.props.stat }</div>)
-              : (<div className="stat blank-line flex-1"></div>) }
+              : (<div className="stat blank-line flex-1">&nbsp;</div>) }
             <div className="rating"><Dots fill={ this.props.rating } max={ 5 } /></div>
         </div>);
 }
+});
+
+var BigTable = React.createClass({
+    propTypes: {
+        columns: React.PropTypes.arrayOf(React.PropTypes.shape({
+            title: React.PropTypes.string,
+            width: React.PropTypes.number
+        })),
+        values: React.PropTypes.arrayOf(React.PropTypes.arrayOf(React.PropTypes.node))
+    },
+
+    render: function () {
+        var self = this;
+        return (<div className="big-table">
+            <header className="big-table-header flex-container">
+                { this.props.columns.map(function(c) {
+                    return (<div key={ c.title } className="flex-inline" style={ {flex: c.width} }>{ c.title }</div>);
+                }) }
+            </header>
+            { this.props.values.map(function(v) {
+                return (<div key={ v.join() } className="big-table-row flex-container">
+                    { v.map(function(c, idx) {
+                        var col = self.props.columns[idx];
+                        return (<div key={ col.title } className="flex-inline" style={ {flex: col.width} }>{ Array.isArray(c) ? c.join(', ') : c }</div>);
+                    }) }
+                </div>);
+            })}
+            <div className="big-table-row flex-container">
+                { this.props.columns.map(function(c) {
+                    return (<div key={ c.title } className="blank-line flex-inline" style={ {flex: c.width} }>&nbsp;</div>);
+                }) }
+            </div>
+        </div>)
+    }
 });
 
 var AttributesPanel = React.createClass({
@@ -225,4 +259,53 @@ render: function () {
       </div>
     </BigPanel>);
 }
+});
+
+var AttacksPanel = React.createClass({
+    propTypes: {
+        attacks: React.PropTypes.objectOf(React.PropTypes.shape({
+            accuracy: React.PropTypes.number,
+            witheringDamage: React.PropTypes.number,
+            decisiveDamage: React.PropTypes.oneOf(['B','L','A']),
+            defense: React.PropTypes.number,
+            overwhelming: React.PropTypes.number,
+            tags: React.PropTypes.arrayOf(React.PropTypes.string),
+            ability: React.PropTypes.string
+        })).isRequired
+    },
+
+    toSigned: function(num) {
+        return num >= 0 ? "+"+num : ""+num;
+    },
+
+    convertToRow: function(name, props) {
+        var acc = (Array.isArray(props.accuracy) ? props.accuracy : [props.accuracy]).map(this.toSigned).join('/');
+        var dmg = [this.toSigned(props.witheringDamage), props.decisiveDamage].join('/');
+        var def = props.defense === null ? "\u2014" : this.toSigned(props.defense);
+        var ovw = ""+props.overwhelming;
+        var tags = props.tags.join(', ');
+        var abil = props.ability;
+        return [name, acc, dmg, def, ovw, tags, abil];
+    },
+
+    render: function () {
+        var self = this;
+        var attacks = Object.assign({}, this.props.attacks, {
+            'Unarmed': {
+                accuracy: 4,
+                witheringDamage: 7,
+                decisiveDamage: 'B',
+                defense: 0,
+                overwhelming: 1,
+                tags: ['Grappling', 'Natural'],
+                ability: 'Brawl'
+            }
+        })
+        var unarmed = [ 'Unarmed', 4, 7, 0, 1, ['Bashing', 'Brawl', 'Grappling', 'Natural'], 'Brawl']
+        return (<BigPanel title="Attacks">
+            <BigTable columns={ [ {title: 'Name', width: 10}, {title: 'Acc', width: 3}, {title: 'Dmg', width: 2},
+                                {title: 'Def', width: 2}, {title: 'Ovw', width: 2}, {title: 'Tags', width: 8}, { title: 'Abil', width: 3} ] }
+                      values={ Object.keys(attacks).sort().map(function(a) { return self.convertToRow(a, attacks[a]); }) } />
+        </BigPanel>);
+    }
 });
