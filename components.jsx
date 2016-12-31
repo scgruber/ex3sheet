b0ac2d2e9896ac153e72b77363c84150f5f97f42
Sam Gruber <grubermensch@gmail.com>
2016-12-31 15:12:18 -0800

    Convert Limit Break to React

--- components.jsx
+++ components.jsx
@@ -295,6 +295,27 @@ var ExperiencePanel = React.createClass({
 }
 });
 
+var LimitBreakPanel = React.createClass({
+    propTypes: {
+        limit: React.PropTypes.shape({
+            permanent: React.PropTypes.number.isRequired,
+            accumulated: React.PropTypes.number.isRequired,
+            trigger: React.PropTypes.string.isRequired
+        })
+    },
+
+    render: function () {
+        return (<BigPanel title="Limit Break" id="limit-break">
+            <div id="limit-accumulated">
+                <Boxes fill={ this.props.limit.accumulated } max={ 10 } enabled={ 10 - this.props.limit.permanent } />
+            </div>
+            <div id="limit-trigger">
+                { this.props.limit.trigger.length === 0 ? <div className="blank-line"> </div> : <div>{ this.props.limit.trigger }</div> }
+            </div>
+        </BigPanel>);
+    }
+});
+
 var HealthLevelsPanel = React.createClass({
     propTypes: {
         health: React.PropTypes.shape({
