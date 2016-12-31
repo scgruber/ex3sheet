b6fe5b0101c0cc18816d2b15937bd851371e0039
Sam Gruber <grubermensch@gmail.com>
2016-12-31 15:59:16 -0800

    Convert Character to React

--- components.jsx
+++ components.jsx
@@ -207,7 +207,57 @@ var HealthLevel = React.createClass({
             <div className="health-level-levels">{ levels }</div>
         </div>)
     }
-})
+});
+
+var LabeledField = React.createClass({
+    propTypes: {
+        label: React.PropTypes.string.isRequired,
+        value: React.PropTypes.node
+    },
+
+    render: function () {
+        return (<div className="flex-container">
+            <div className="labeled-field-label flex-1">{ this.props.label }</div>
+            { this.props.value ?
+                (<div className="labeled-field-value flex-2">{ this.props.value }</div>)
+                : (<div className="labeled-field-value blank-line flex-2">&nbsp;</div>) }
+        </div>)
+    }
+});
+
+var CharacterPanel = React.createClass({
+    propTypes: {
+        name: React.PropTypes.string,
+        player: React.PropTypes.string,
+        type: React.PropTypes.oneOf([
+            'Solar Exalted',
+        ]),
+        caste: React.PropTypes.oneOf([ 'Dawn', 'Zenith', 'Twilight', 'Night', 'Eclipse' ]),
+        concept: React.PropTypes.string,
+        totem: React.PropTypes.string,
+        abilities: React.PropTypes.objectOf(React.PropTypes.shape({
+            supernal: React.PropTypes.boolean
+        }))
+    },
+
+    supernal: function () {
+        var self = this;
+        return Object.keys(this.props.abilities).find(function(a) { return self.props.abilities[a].supernal === true; });
+    },
+
+    render: function () {
+        return (<div>
+            <header id="name"><h1>{ this.props.name }</h1></header>
+            <LabeledField label="Player" value={ this.props.player } />
+            <div id="portrait" />
+            <LabeledField label="Type" value={ this.props.type } />
+            <LabeledField label="Caste" value={ this.props.caste } />
+            <LabeledField label="Concept" value={ this.props.concept } />
+            <LabeledField label="Totem" value={ this.props.totem } />
+            <LabeledField label="Supernal" value={ this.supernal() } />
+        </div>);
+    }
+});
 
 var EssencePanel = React.createClass({
     propTypes: {
