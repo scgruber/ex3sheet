adeef17fb6ac4747dd861c1688d3c620b3de4e3a
Sam Gruber <grubermensch@gmail.com>
2017-01-09 21:14:11 -0800

    Break out EssencePanel into a separate file

--- jsx/components.jsx
+++ jsx/components.jsx
@@ -44,59 +44,7 @@ var CharacterPanel = React.createClass({
     }
 });
 
-var EssencePanel = React.createClass({
-    propTypes: {
-        essence: React.PropTypes.shape({
-            personal: React.PropTypes.shape({
-                free: React.PropTypes.number
-            }),
-            peripheral: React.PropTypes.shape({
-                free: React.PropTypes.number
-            })
-        }),
-        experience: React.PropTypes.shape({
-            general: React.PropTypes.shape({
-                total: React.PropTypes.number
-            })
-        }),
-        artifacts: React.PropTypes.objectOf(React.PropTypes.shape({
-            attune: React.PropTypes.shape({
-                motes: React.PropTypes.number,
-                type: React.PropTypes.oneOf(['Personal', 'Peripheral'])
-            })
-        }))
-    },
-
-    committed: function(type) {
-        var self = this;
-        return Object.keys(this.props.artifacts).filter(function(a) { return self.props.artifacts[a].attune.type === type; })
-            .map(function(a) { return self.props.artifacts[a].attune.motes; }).reduce(function(acc, e) { return acc+e; }, 0);
-    },
-
-    poolBreakdown: function(pool, props, committed) {
-        return props.free + ' / ' + (pool - committed) + ' [' + committed + ']';
-    },
-
-    render: function () {
-        var self = this;
-        var essenceThresholds = [0, 50, 125, 200, 300];
-        var essence = essenceThresholds.findIndex(function(t) { return self.props.experience.general.total < t; });
-        if (essence == -1) essence = 5;
-
-        var personalPool = essence*3 + 10;
-        var peripheralPool = essence*7 + 26;
-
-        return (<BigPanel title="Essence" id="essence">
-            <div id="essence-rating">
-                <Dots fill={ essence } max={ 5 }/>
-            </div>
-            <LittleTable columns={ ['Personal', 'Peripheral'] }
-                         values={ [ this.poolBreakdown(personalPool, this.props.essence.personal, this.committed('Personal')),
-                                    this.poolBreakdown(peripheralPool, this.props.essence.peripheral, this.committed('Peripheral')) ]} />
-        </BigPanel>);
-    }
-});
-
+var EssencePanel = require('./panels/essence');
 var WillpowerPanel = require('./panels/willpower');
 var ExperiencePanel = require('./panels/experience');
 var LimitBreakPanel = require('./panels/limit_break');
