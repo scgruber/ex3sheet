00e4ac33fe761f3d24b60beb63a37a6c0f52d755
Sam Gruber <grubermensch@gmail.com>
2017-01-09 21:21:15 -0800

    Break out CharacterPanel into a separate file

--- jsx/components.jsx
+++ jsx/components.jsx
@@ -10,40 +10,7 @@ var BigTable = require('./components/big_table');
 var LittleTable = require('./components/little_table');
 var LabeledField = require('./components/labeled_field');
 
-var CharacterPanel = React.createClass({
-    propTypes: {
-        name: React.PropTypes.string,
-        player: React.PropTypes.string,
-        type: React.PropTypes.oneOf([
-            'Solar Exalted',
-        ]),
-        caste: React.PropTypes.oneOf([ 'Dawn', 'Zenith', 'Twilight', 'Night', 'Eclipse' ]),
-        concept: React.PropTypes.string,
-        totem: React.PropTypes.string,
-        abilities: React.PropTypes.objectOf(React.PropTypes.shape({
-            supernal: React.PropTypes.boolean
-        }))
-    },
-
-    supernal: function () {
-        var self = this;
-        return Object.keys(this.props.abilities).find(function(a) { return self.props.abilities[a].supernal === true; });
-    },
-
-    render: function () {
-        return (<div>
-            <header id="name"><h1>{ this.props.name }</h1></header>
-            <LabeledField label="Player" value={ this.props.player } />
-            <div id="portrait" />
-            <LabeledField label="Type" value={ this.props.type } />
-            <LabeledField label="Caste" value={ this.props.caste } />
-            <LabeledField label="Concept" value={ this.props.concept } />
-            <LabeledField label="Totem" value={ this.props.totem } />
-            <LabeledField label="Supernal" value={ this.supernal() } />
-        </div>);
-    }
-});
-
+var CharacterPanel = require('./panels/character');
 var EssencePanel = require('./panels/essence');
 var WillpowerPanel = require('./panels/willpower');
 var ExperiencePanel = require('./panels/experience');
